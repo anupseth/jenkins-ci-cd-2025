a4ff875d934ebf5dc8004617d98e73dd6a181a3c
Anupkumar Seth <anupseth11@yahoo.com>
2025-01-27 21:23:33 +0100

    intitial commit

---
+++ greetingsapp/src/main/java/com/example/greetingsapp/GreetingsappApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingsappApplication.class, args);
+	}
+
+}
---
+++ greetingsapp/src/test/java/com/example/greetingsapp/GreetingsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
